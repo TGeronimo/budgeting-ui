c7a03e94c0b11d16438b22a52f84fe31926ad910
Thiago Geronimo <thi.matheus@gmail.com>
2026-09-03 11:21:26 -0300

    Create token storage logic with Flutter Secure Storage

--- lib/features/auth/services/token_storage.dart
+++ lib/features/auth/services/token_storage.dart
@@ -2,4 +2,24 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenStorage {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<void> saveToken(String token) async {
+    await _storage.write(key: 'token', value: token);
+  }
+
+  Future<void> saveRefreshToken(String refreshToken) async {
+    await _storage.write(key: 'refreshToken', value: refreshToken);
+  }
+
+  Future<String?> getToken() async {
+    await _storage.read(key: 'token');
+  }
+
+  Future<String?> getRefreshToken() async {
+    await _storage.read(key: 'refreshToken');
+  }
+
+  Future<void> clear() async {
+    await _storage.deleteAll();
+  }
 }
