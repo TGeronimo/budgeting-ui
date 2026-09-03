49f5d96adfd11e78cc2eb4485f46a0eb44b4e466
Thiago Geronimo <thi.matheus@gmail.com>
2026-09-03 19:02:49 -0300

    Add onError and _refreshToken to interceptor and add interceptor to Dio.

--- lib/core/dio/dio_client.dart
+++ lib/core/dio/dio_client.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_app_test/features/auth/services/auth_interceptor.dart';
+import 'package:flutter_app_test/features/auth/services/token_storage.dart';
 
 class DioClient {
   final Dio dio;
+  final TokenStorage tokenStorage;
 
-  DioClient()
+  DioClient(this.tokenStorage)
     : dio = Dio(
         BaseOptions(
           baseUrl: 'http://localhost:8080',
@@ -11,15 +14,21 @@ class DioClient {
           receiveTimeout: const Duration(seconds: 5),
           headers: {
             'Content-Type': 'application/json',
-          },
-        ),
-      ) {
-  // Interceptor de log (opcional, mas muito útil)
-  dio.interceptors.add(
-    LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-    ),
-  );
-  }
+            },
+          ),
+        ) {
+          // Interceptor de log
+          dio.interceptors.add(
+            LogInterceptor(
+              requestBody: true,
+              responseBody: true,
+            ),
+          );
+          // Interceptor de tokens
+          dio.interceptors.add(
+            AuthInterceptor(
+              tokenStorage, 
+              dio)
+          );
+        }
 }
