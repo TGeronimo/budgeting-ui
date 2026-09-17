bae1c3c2d366fa123d3ceaa392142879052a474d
Thiago Geronimo <thi.matheus@gmail.com>
2026-09-17 18:19:49 -0300

    Mock state transitions in register transaction page.

--- lib/features/transactions/pages/register_transaction_page.dart
+++ lib/features/transactions/pages/register_transaction_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter_app_test/features/auth/pages/enum_transaction_page_state
 
 import 'package:record/record.dart';
 
+import '../widgets/error_state_widget.dart';
+import '../widgets/idle_state_widget.dart';
+import '../widgets/playing_state_widget.dart';
+import '../widgets/processing_state_widget.dart';
+import '../widgets/recording_state_widget.dart';
+
 class RegisterTransactionPage extends StatefulWidget {
 
   const RegisterTransactionPage({super.key});
@@ -14,30 +20,61 @@ class RegisterTransactionPage extends StatefulWidget {
 }
 
 class _RegisterTransactionPageState extends State<RegisterTransactionPage> {
-  EnumTransactionPageState currentState = EnumTransactionPageState.idle;
+  EnumTransactionPageState _currentState = EnumTransactionPageState.idle;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.mic_rounded),
-          SizedBox(height: 16),
-          ElevatedButton(
-              onPressed: () {
-
-              },
-              child: Text('Gravar'),
-          ),
-          SizedBox(height: 16),
-          ElevatedButton(
-              onPressed: () {},
-              child: Text('Parar'))
-          ],
-      ),
+      body:
+      switch (_currentState) {
+        EnumTransactionPageState.idle => IdleStateWidget(),
+        EnumTransactionPageState.recording => RecordingStateWidget(),
+        EnumTransactionPageState.processing => ProcessingStateWidget(),
+        EnumTransactionPageState.playing => PlayingStateWidget(),
+        EnumTransactionPageState.error => ErrorStateWidget(),
+      },
+      persistentFooterButtons: [
+        FloatingActionButton.small(
+            onPressed: () {
+                setState(() {
+                  _currentState = EnumTransactionPageState.recording;
+                });
+        },
+        child: Icon(Icons.mic_rounded),
+        ),
+        FloatingActionButton.small(
+            onPressed: () {
+                setState(() {
+                  _currentState = EnumTransactionPageState.playing;
+                });
+        },
+        child: Icon(Icons.play_arrow_rounded),
+        ),
+        FloatingActionButton.small(
+            onPressed: () {
+                setState(() {
+                  _currentState = EnumTransactionPageState.processing;
+                });
+        },
+        child: Icon(Icons.check_rounded),
+        ),
+        FloatingActionButton.small(
+            onPressed: () {
+                setState(() {
+                  _currentState = EnumTransactionPageState.error;
+                });
+        },
+        child: Icon(Icons.error_rounded),
+        ),
+        FloatingActionButton.small(
+            onPressed: () {
+                setState(() {
+                  _currentState = EnumTransactionPageState.idle;
+                });
+        },
+        child: Icon(Icons.close_rounded),
+        ),
+      ],
     );
-
   }
 }
